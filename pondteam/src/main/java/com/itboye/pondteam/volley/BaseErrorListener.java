25925f940d51682b9d71a1de6a63746145917c3d
abc123456 <itboye@163.com>
2018-01-20 16:20:30 +0800

    错误捕获异常更新

--- pondteam/src/main/java/com/itboye/pondteam/volley/BaseErrorListener.java
+++ pondteam/src/main/java/com/itboye/pondteam/volley/BaseErrorListener.java
@@ -2,8 +2,14 @@ package com.itboye.pondteam.volley;
 
 import android.content.Intent;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkError;
+import com.android.volley.NoConnectionError;
+import com.android.volley.ParseError;
 import com.android.volley.ServerError;
+import com.android.volley.TimeoutError;
 import com.android.volley.VolleyError;
+import com.itboye.pondteam.R;
 import com.itboye.pondteam.app.MyApplication;
 import com.itboye.pondteam.utils.Const;
 
@@ -17,7 +23,6 @@ public class BaseErrorListener implements XErrorListener {
 
     @Override
     public void onErrorResponse(Exception exception, String code, String msg) {
-//			Log.v("error", exception.getMessage());
         if (listener != null) {
             if (code == "1111") {
                 Intent intent = new Intent();
@@ -32,20 +37,33 @@ public class BaseErrorListener implements XErrorListener {
                 if (exception != null) {
                     msg = exception.getMessage();
                 } else {
-                    msg = "发生未知错误!";
+                    msg =  MyApplication.getInstance().getString(R.string.server_error_unknow);
                 }
             }
             if (exception instanceof ServerError) {
                 ServerError error = (ServerError) exception;
                 code = "" + error.networkResponse.statusCode;
-                msg = "服务器返回错误!";
+                msg =  MyApplication.getInstance().getString(R.string.server_error);
 
-            }
-
-            if (exception instanceof VolleyError) {
+            } else if (exception instanceof VolleyError) {
                 VolleyError jsonException = (VolleyError) exception;
-                String error = jsonException.getLocalizedMessage();
-                msg = "服务器开小差了..." + error;
+                String error = jsonException.getMessage();
+                if (jsonException instanceof TimeoutError) {
+                    msg = MyApplication.getInstance().getString(R.string.volley_error_timeout);
+                } else if (jsonException instanceof NoConnectionError) {
+                    msg = MyApplication.getInstance().getString(R.string.volley_error_noconnect);
+                } else if (jsonException instanceof AuthFailureError) {
+                    msg = MyApplication.getInstance().getString(R.string.volley_error_authfailure);
+                } else if (jsonException instanceof NetworkError) {
+                    msg = MyApplication.getInstance().getString(R.string.volley_error_networkerror);
+                } else if (jsonException instanceof ParseError) {
+                    msg = MyApplication.getInstance().getString(R.string.volley_error_parseerror);
+                } else if (jsonException instanceof ServerError) {
+                    msg = MyApplication.getInstance().getString(R.string.volley_error_servererror);
+                } else {
+                    msg = MyApplication.getInstance().getString(R.string.volley_error_server) + jsonException;
+                }
+
             }
             ResultEntity result = new ResultEntity(Integer.parseInt(code), msg, exception);
             listener.failure(result);
